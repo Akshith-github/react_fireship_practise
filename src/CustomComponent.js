864b2bf1b78a3f8fec9227f78ac41fa1db40d06c
Akshith <akshithnukala9@gmail.com>
2024-03-18 10:34:03 -0400

    chap 1.1 Into to StateHooks with useState

--- src/CustomComponent.js
+++ src/CustomComponent.js
@@ -1,9 +1,16 @@
 import React from  'react';
+import {useState} from "react";
 
 export default function Item(props){
+    const [count,setCount] = useState(0);
     return (
+        <>
         <p>
             {props.text}
+            this component is clicked  {count} times
         </p>
+        <button onClick={()=>{setCount(count+1)}}>click me</button>
+        </>
+        
     );
 }
